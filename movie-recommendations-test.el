30d46219179b031902faa754f9249734ffbd6a76
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-22 21:08:45 +0200

    Add sub optimal (and noisy) test setup

--- movie-recommendations-test.el
+++ movie-recommendations-test.el
@@ -1,7 +1,18 @@
 (require 'buttercup)
 (setq load-path (cons "." load-path))
 (require 'movie-recommendations)
+(require 'mock-server)
+
+(defun stop-servers ()
+  (dolist (port (elnode-ports))
+    (elnode-stop port)))
 
 (describe "movie-recomendations"
-  (it "exists and load"
+  (before-each
+    (start-mock-server))
+  (after-each
+    (stop-servers))
+  (it "exists and load (1)"
+    (expect t :to-be t))
+  (it "exists and load (2)"
     (expect t :to-be t)))
