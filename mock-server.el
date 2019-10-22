9ecfe02279af9e6faf2597300e4970468cec576b
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-22 20:51:32 +0200

    Create a passable mock server

--- mock-server.el
+++ mock-server.el
@@ -15,21 +15,32 @@
   (elnode-http-start httpcon 200 '("Content Type" . "application/json"))
   (elnode-http-return httpcon (format "path: %s\nparameters: %s" (elnode-http-pathinfo httpcon) (elnode-http-params httpcon))))
 
-(defconst mock-server--result '("search-result"  "jurassic_park_example.json"
-                                "search-not-found"  "movie_not_found.json"
-                                "invalid-key"  "invalid_api_key.json"
-                                "no-key"  "no_api_key.json"))
+(defconst mock-server--result '(search-result  "jurassic_park_example.json"
+                                search-not-found  "movie_not_found.json"
+                                invalid-key "invalid_api_key.json"
+                                no-key  "no_api_key.json"))
 
 (defun mock-server--get-sample (response-type)
-  (plist-get)
-  (with-current-buffer (get-file-buffer "sample_results/jurassic_park_example.json")
-                                (buffer-substring (point-min) (point-max))))
+  (let ((file-to-load (concat (file-name-as-directory "sample_results")
+                              (plist-get mock-server--result response-type))))
+    (with-temp-buffer
+      (insert-file-contents file-to-load)
+      (buffer-substring-no-properties (point-min) (point-max)))))
+
+(defun mock-server--select-content (httpcon)
+  (mock-server--get-sample
+   (let ((api-key (elnode-http-param httpcon "apikey"))
+         (search (elnode-http-param httpcon "t")))
+     (cond
+      ((null api-key) 'no-key)
+      ((not (equal "API-KEY" api-key)) 'invalid-key)
+      ((not (equal "jurassic" search)) 'search-not-found)
+      ((equal "jurassic" search) 'search-result)
+      (t "unexpected condition")))))
 
 (defun mock-imdb-handler (httpcon)
   (elnode-http-start httpcon 200 '("Content Type" . "application/json"))
-  (elnode-http-return httpcon (mock-server--get-sample "search_result")))
-
-
+  (elnode-http-return httpcon (mock-server--select-content httpcon)))
 
 (defun stop-mock-server ()
   (condition-case nil
