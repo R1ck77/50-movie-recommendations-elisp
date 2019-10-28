29483cd3f47fec96859b446897648de9f59b2376
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-10-28 23:07:53 +0100

    Minor refactoring
    …to make paredit happy? Sad :(

--- movie-recommendations-formatter-test.el
+++ movie-recommendations-formatter-test.el
@@ -3,6 +3,28 @@
 (setq load-path (cons "." load-path))
 (require 'movie-recommendations-formatter)
 
+(defvar jurassic-park-representation "Title: Jurassic Park
+
+[http://somewhere:80/jurassic.jpg]
+
+Year: 1993
+Rated: PG-13
+Running Time: 127 min
+Plot: A pragmatic Paleontologist visiting an almost complete theme park is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.
+
+You should watch this movie right now!")
+
+(defvar alone-in-the-dark-representation "Title: Alone in the Dark
+
+[http://somewhere:80/alone.jpg]
+
+Year: 2005
+Rated: R
+Running Time: 96 min
+Plot: A detective of the paranormal slowly unravels mysterious events with deadly results.
+
+This is a bad movie, avoid it if you can!")
+
 (defun read-json (name)
   (json-read-from-string
    (format (with-temp-buffer
@@ -17,25 +39,9 @@
   (describe "movie-recommendations-format-data"
     (it "formats the json data for a positive review"
       (expect (movie-recommendations-format-data jurassic-json)
-              :to-equal "Title: Jurassic Park
-
-[http://somewhere:80/jurassic.jpg]
-
-Year: 1993
-Rated: PG-13
-Running Time: 127 min
-Plot: A pragmatic Paleontologist visiting an almost complete theme park is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.
-
-You should watch this movie right now!"))
+              :to-equal jurassic-park-representation))
     (it "formats the json data for a negative review"
       (expect (movie-recommendations-format-data alone-json)
-              :to-equal "Title: Alone in the Dark
-
-[http://somewhere:80/alone.jpg]
-
-Year: 2005
-Rated: R
-Running Time: 96 min
-Plot: A detective of the paranormal slowly unravels mysterious events with deadly results.
-
-This is a bad movie, avoid it if you can!"))))
+              :to-equal alone-in-the-dark-representation))
+    (it  "it embeds the poster image"
+      )))
