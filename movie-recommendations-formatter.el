cfa133478556bdcd8184d47cf39d6e1c93842bd7
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-11-04 10:29:41 +0100

    Make the poster show up (!tests)
    This required a basic image downloading in place, which in turn brings a basic cache into play almost for free

--- movie-recommendations-formatter.el
+++ movie-recommendations-formatter.el
@@ -7,20 +7,29 @@ A less manichean function would be of course more realistic"
       "You should watch this movie right now!"
     "This is a bad movie, avoid it if you can!"))
 
-(defun movie-recommendations--image (url)
-  (with-temp-buffer
-    (insert-image (create-image url nil nil nil) (concat "[" url "]"))
-    (buffer-substring (point-min) (point-max))))
+(defun get-image (id url)
+  (let ((image-path (concat (file-name-as-directory (concat (file-name-as-directory (file-truename ".")) "cache/images")) (concat id ".jpg"))))
+    (if (not (file-exists-p image-path))
+        (url-utils-download-image url image-path))
+    image-path))
+
+(defun movie-recommendations--image (id url title)
+  (let ((image-path (get-image id url))
+        (image-alt (format "\[poster for %s\]" title)))
+    (put-text-property 0 (length image-alt) 'display (list 'image :file image-path :type 'jpeg) image-alt)
+    image-alt))
 
 (defun movie-recommendations-format-data (json)
   (let ((title (alist-get 'Title json))
-        (poster (movie-recommendations--image (alist-get 'Poster json)))
         (year (alist-get 'Year json))
         (rating (alist-get 'Rated json))
         (length (alist-get 'Runtime json))
         (plot (alist-get 'Plot json))
         (imdb-rating (string-to-number (alist-get 'imdbRating json))))
-    (format "Title: %s
+    (let ((poster (movie-recommendations--image (alist-get 'imdbID json)
+                                                (alist-get 'Poster json)
+                                                title)))
+      (format "Title: %s
 
 %s
 
@@ -31,7 +40,7 @@ Plot: %s
 
 %s" title poster year rating
 length plot
-(movie-recommendations--evaluation imdb-rating))))
+(movie-recommendations--evaluation imdb-rating)))))
 
 
 (provide 'movie-recommendations-formatter)
